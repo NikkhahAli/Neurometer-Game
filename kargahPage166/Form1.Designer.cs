9ce09edcb405c17192634d623e948329e9f2c2f0
Ali nikkhah <aliii.nikkhah@gmail.com>
2025-03-07 17:02:21 +0330

    first

---
+++ kargahPage166/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace kargahPage166
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(30, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(972, 575);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(380, 464);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "end";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button3_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(991, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 433);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(731, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 48);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(731, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 212);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(555, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 64);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Location = new System.Drawing.Point(555, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 212);
+            this.label5.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label6.Location = new System.Drawing.Point(384, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 58);
+            this.label6.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label7.Location = new System.Drawing.Point(384, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 284);
+            this.label7.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(717, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 36);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(161, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 29);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(25, 155);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(92, 29);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1418, 754);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ kargahPage166/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kargahPage166
+{
+    public partial class Form1 : Form
+    {
+        bool isActiveGame = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        int seconds = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label9.Text == "label9")
+            {
+                MessageBox.Show("قبل از شروع بازی باید یک اسم انتخاب کنید");
+            }
+            else
+            {
+                isActiveGame = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label9.Text = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isActiveGame)
+            {
+                seconds++;
+                label8.Text = seconds.ToString();
+                timer1.Start();
+            }
+        }
+
+        bool showMessage = false;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isActiveGame)
+            {
+                for (int i = 1; i <= 7; i++)
+                {
+                    if (Controls["label" + i].Location != e.Location)
+                    {
+                        isActiveGame = false;
+                        if (isActiveGame == false)
+                        {
+                            timer1.Stop();
+                            MessageBox.Show("شما باختید");
+                            isActiveGame = false;
+                            label10.Text = "" + seconds;
+                            break;
+                        }
+                    }
+                   
+                }
+            }
+        }
+
+        private void button3_MouseMove(object sender, MouseEventArgs e)
+        {
+            for (int k = 0; k < 1; k++)
+            {
+                isActiveGame = false;
+                if (isActiveGame == false)
+                {
+                    timer1.Stop();
+                    label10.Text = "" + seconds;
+                    MessageBox.Show("شما بردید", "" + k);
+                    break;
+                }
+            }
+        }
+    }
+}
